a7187f977c404d1a05b4510fc473be9433d8bcfe
Henry Augusta <henryaugusta4@gmail.com>
2021-09-21 10:51:54 +0700

    Fix Java 11

--- app/src/main/java/com/feylabs/razkyutility/MainActivity.kt
+++ app/src/main/java/com/feylabs/razkyutility/MainActivity.kt
@@ -2,10 +2,16 @@ package com.feylabs.razkyutility
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.firriezky.razkyutility.Toaster
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        Toaster().showShortToast(this,"Hello Gais")
+
     }
 }
